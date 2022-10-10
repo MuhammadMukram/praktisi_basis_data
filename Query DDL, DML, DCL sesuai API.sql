4795e75cf35acddc49f14cd83c78e644095f28c4
Mukramm_ <mukrammuhammad@gmail.com>
2022-10-10 23:05:48 +0800

    Tambahkan template baru yang sesuai dan perbaikan query

---
+++ Query DDL, DML, DCL sesuai API.sql
@@ -0,0 +1,20 @@
+-- Create new student info record : Attar
+
+
+-- List students : Attar
+
+
+-- Update student info record : Mukram
+
+
+-- List of schedule on specific student : Mukram
+
+
+-- List of courses : Arif
+
+
+-- Create / input new schedule for specific student : Arif
+
+
+-- Edit /update schedule for specific student : Fitrah
+
