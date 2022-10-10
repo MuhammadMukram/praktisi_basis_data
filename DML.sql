6354dce551e61794c69d76619319513b9e3f011e
Rexhark <105967519+Rexhark@users.noreply.github.com>
2022-10-11 00:31:16 +0800

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,27 +1,27 @@
 USE tugas_basis_data;
 INSERT INTO students VALUES 
-(1, "Muhammad Arif", "Belibis Street", "Sistem Informasi"),
-(2, "Dummy Siswa1", "Dummy Street1", "Sistem Informasi"),
-(3, "Dummy Siswa2", "Dummy Street2", "Sistem Informasi"),
-(4, "Dummy Siswa3", "Dummy Street3", "Sistem Informasi");
+('H071211004', 'Muhammad Arif', 'Belibis Street', 'Sistem Informasi'),
+('H071211012', 'Dummy 1', 'Dummy Street', 'Sistem Informasi'),
+('H071211017', 'Dummy 2', 'Dummy Street', 'Sistem Informasi'),
+('nim_kalia1', 'Dummy 3', 'Dummy Street', 'Sistem Informasi'),
+('nim_kalia2', 'Dummy 4', 'Dummy Street', 'Sistem Informasi'),
+('nim_kalia3', 'Dummy 5', 'Dummy Street', 'Sistem Informasi'),
+('nim_kalia4', 'Dummy 6', 'Dummy Street', 'Sistem Informasi');
 
 INSERT INTO course VALUES
-(1, "Aljabar Linear", 90),
-(2, "Programming Web", 90),
-(3, "Basis Data", 90),
-(4, "Anilisis Perancangan", 90);
+('21H0712010','Struktur Diskrit', ),
+('21H0712020','Aljabar Linier Terapan', ),
+('21H0712030','Struktur Data', ),
+('21H0712040','Sistem Basis Data I', ),
+('21H0712050','Praktikum Sistem Basis Data I', ),
+('21H0712060','Pemrograman Web', ),
+('21H0712070','Praktikum Pemrograman Web', ),
+('21H0712080','Analisis dan Perancangan Sistem', ),
+('21H0712090','Praktikum Analisis dan Perancangan Sistem', ),
+('21H0712100','Metode Statistika', );
 
-INSERT INTO jadwal VALUES
+INSERT INTO schedule VALUES
 (1, "Aljabar Linear", '2022-1-12', '07:30', 1, 1),
 (2, "Programming Web", '2022-1-12', '08:30', 1, 2),
 (3, "Basis Data", '2022-1-12', '11:00', 1, 3),
 (4, "Analisis Perancangan", '2022-1-12', '13:30', 1, 4);
-
-
--- Get API List of courses
-SELECT * FROM jadwal;
-
--- Create / input new schedule for specific student
-INSERT INTO jadwal VALUES (12, 'Analisis Perancangan', 2022-10-03, 13:30:00 3, 4;
-
-                           
