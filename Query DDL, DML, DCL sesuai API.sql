0a153b4f571372a4c89e3c20749bfbf4742f09c9
Attarmusharih <114406161+Attarmusharih@users.noreply.github.com>
2022-10-10 09:57:52 -0600

    Update tabel

--- Query DDL, DML, DCL sesuai API.sql
+++ Query DDL, DML, DCL sesuai API.sql
@@ -3,16 +3,16 @@
 USE db_klp1B;
 
 -- Create new student info record : Attar
-
-CREATE TABLE `course` (
-  `course_id` int(11) NOT NULL,
-  `NAME` varchar(255) NOT NULL,
-  `duration` int(11) NOT NULL,
-  PRIMARY KEY (`course_id`)
-)
+-- CREATE TABLE `students` (
+--   `students_id` int(11) NOT NULL,
+--   `full_name` varchar(255) DEFAULT NULL,
+--   `address` varchar(255) DEFAULT NULL,
+--   `major` varchar(255) DEFAULT NULL,
+--   PRIMARY KEY (`students_id`)
+-- )
 
 -- List students : Attar
-
+SELECT * FROM students;
 
 -- Update student info record : Mukram
 
