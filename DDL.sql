a55b07f551f972e71e3ac7031158a0caa711da98
Rexhark <105967519+Rexhark@users.noreply.github.com>
2022-10-11 00:19:03 +0800

    Update DDL.sql
    Mengganti beberapa nama, tipe data, dan panjang field.

--- DDL.sql
+++ DDL.sql
@@ -1,30 +1,29 @@
 CREATE DATABASE tugas_basis_data;
 USE tugas_basis_data;
 CREATE TABLE `course` (
-  `course_id` int(11) NOT NULL AUTO INCREMENT,
-  `NAME` varchar(255) NOT NULL,
-  `duration` int(11) NOT NULL,
+  `course_id` VARCHAR(10) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `duration` INT(11) NOT NULL,
   PRIMARY KEY (`course_id`)
-)
-
-CREATE TABLE `jadwal` (
-  `jadwal_id` int(11) NOT NULL AUTO INCREMENT,
-  `nama` varchar(255) NOT NULL,
-  `tanggal` date NOT NULL,
-  `jam` time NOT NULL,
-  `students_id` int(11) DEFAULT NULL,
-  `course_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `students_id` (`students_id`),
-  KEY `course_id` (`course_id`),
-  CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`students_id`) REFERENCES `students` (`students_id`),
-  CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`)
-)
+);
 
 CREATE TABLE `students` (
-  `students_id` varchar(11) NOT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `major` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`students_id`)
-)
+  `student_id` VARCHAR(10) NOT NULL,
+  `full_name` VARCHAR(255) DEFAULT NULL,
+  `address` VARCHAR(255) DEFAULT NULL,
+  `major` VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
+);
+
+CREATE TABLE `schedule` (
+  `schedule_id` INT(11) AUTO_INCREMENT NOT NULL,
+  `tanggal` DATE NOT NULL,
+  `jam` TIME NOT NULL,
+  `student_id` VARCHAR(10) NOT NULL,
+  `course_id` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`schedule_id`),
+  KEY `student_id` (`schedule_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`)
+);
