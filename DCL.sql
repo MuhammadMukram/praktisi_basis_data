c0ef8170d4e72b6bfc34ffbbff7f121eb3b363eb
Attarmusharih <114406161+Attarmusharih@users.noreply.github.com>
2022-10-10 10:08:57 -0600

    Update DCL.sql

--- DCL.sql
+++ DCL.sql
@@ -6,3 +6,11 @@ GRANT INSERT,UPDATE,DELETE,SELECT ON tugas_basis_data.* TO 'dosen'@'localhost';
 
 CREATE USER 'mahasiswa'@'localhost' IDENTIFIED BY 'siswa';
 GRANT SELECT ON tugas_basis_data.* TO 'mahasiswa'@'localhost' FLUSH;
+
+
+/*
+Jadi dibuatkan 3 user dikarenakan untuk menjadi contoh jika user dosen maka dia bisa CRUD kedalam 
+database dimana CRUD digunakan untuk melihat, mengubah, menghapus, dan membuat data baru dan untuk 
+mahasiswa diberikan previlage untuk melihat database saja sedangkan untuk developer mendapatkan semua akses command sql 
+di database ini saja.
+*/
